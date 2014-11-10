532a16c3bebaff970ecf42986f46b6db6032ae99
arahuja <aahuja11@gmail.com>
2014-11-10 10:31:37 -0500

    more benchmarking

--- src/test/scala/org/bdgenomics/guacamole/benchmarks/GenotypeLikelihoodBenchmark.scala
+++ src/test/scala/org/bdgenomics/guacamole/benchmarks/GenotypeLikelihoodBenchmark.scala
@@ -1,8 +1,7 @@
 package org.bdgenomics.guacamole.benchmarks
 
-import org.bdgenomics.guacamole.TestUtil.HasSparkContext
-import org.bdgenomics.guacamole.pileup.Pileup
-import org.bdgenomics.guacamole.{DistributedUtil, LociSet, TestUtil}
+import org.bdgenomics.guacamole.TestUtil
+import org.bdgenomics.guacamole.TestUtil._
 import org.scalameter.PerformanceTest
 import org.scalameter.api._
 
@@ -15,17 +14,10 @@ class GenotypeLikelihoodBenchmark extends PerformanceTest.Quickbenchmark with Ha
   val tumorReads = TestUtil.loadReads(sc, "tumor.chr20.tough.sam")
   val overlapLociReads = tumorReads.mappedReads.filter(_.overlapsLocus(3453543))
 
+
   performance of "Allele" in {
     measure method "computeLikelihoods" in {
-      using(arrays) in {
-        loci => {
-          DistributedUtil.pileupFlatMap[Pileup](
-            tumorReads.mappedReads,
-            DistributedUtil.partitionLociUniformly(4, LociSet.parse(loci)),
-            false, // don't skip empty pileups
-            pileup => Seq(pileup).iterator).collect()
-        }
-      }
+
     }
   }
 

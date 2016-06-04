ef0fd4a93acd2f6911ecda5ba885ec7939728ce8
Ryan Williams <ryan.blake.williams@gmail.com>
2016-06-04 16:27:15 +0000

    convert readsets test to string reads

--- src/test/scala/org/hammerlab/guacamole/readsets/ReadSetsSuite.scala
+++ src/test/scala/org/hammerlab/guacamole/readsets/ReadSetsSuite.scala
@@ -3,8 +3,10 @@ package org.hammerlab.guacamole.readsets
 import com.esotericsoftware.kryo.Kryo
 import org.bdgenomics.adam.models.{SequenceDictionary, SequenceRecord}
 import org.hammerlab.guacamole.reads.{MappedRead, Read}
-import org.hammerlab.guacamole.reference.ReferenceBroadcast
+import org.hammerlab.guacamole.readsets.Util.{TestPileup, simplifyPileup}
 import org.hammerlab.guacamole.reference.ReferenceBroadcast.MapBackedReferenceSequence
+import org.hammerlab.guacamole.reference.ReferencePosition.Locus
+import org.hammerlab.guacamole.reference.{Contig, ReferenceBroadcast}
 import org.hammerlab.guacamole.util.{Bases, GuacFunSuite, KryoTestRegistrar, TestUtil}
 
 import scala.collection.mutable
@@ -71,24 +73,12 @@ class ReadSetsSuite extends GuacFunSuite with Util {
       ).toMap
     )
 
-  type TestRead = (String, Int, Int, Int)
-  type TestReads = Seq[TestRead]
-  type TestPos = (String, Int)
-  type ExpectedReadIdx = (Int, Int)
-  type ExpectedReadIdxs = Iterable[ExpectedReadIdx]
-  type ExpectedReadIdxs3 = (ExpectedReadIdxs, ExpectedReadIdxs, ExpectedReadIdxs)
-
-  type ExpectedPosReads = (TestPos, ExpectedReadIdxs)
-
-  type ExpectedPosReadsPerSample = (TestPos, PerSample[ExpectedReadIdxs])
-  type ExpectedPosPerSample = (TestPos, ExpectedReadIdxs3)
-
-  def makeReadSets(reads: TestReads, numPartitions: Int): (ReadSets, Seq[Read]) = {
+  def makeReadSets(reads: TestReads, numPartitions: Int): (ReadSets, Seq[MappedRead]) = {
     val (readsets, allReads) = makeReadSets(Vector(reads), numPartitions)
     (readsets, allReads(0))
   }
 
-  def makeReadSets(reads: PerSample[TestReads], numPartitions: Int): (ReadSets, PerSample[Seq[Read]]) = {
+  def makeReadSets(reads: PerSample[TestReads], numPartitions: Int): (ReadSets, PerSample[Seq[MappedRead]]) = {
     val (readsRDDs, allReads) =
       (for {
         (sampleReads, sampleId) <- reads.zipWithIndex
@@ -106,14 +96,44 @@ class ReadSetsSuite extends GuacFunSuite with Util {
               start = start,
               chr = contig,
               sampleId = sampleId
-            ): Read
+            )
 
-        val rdd = sc.parallelize(mappedReads, numPartitions)
+        val reads = mappedReads.map(x => x: Read)
+
+        val rdd = sc.parallelize(reads, numPartitions)
 
         ReadsRDD(rdd, "test", contigLengths) -> mappedReads
       }).unzip
 
     (ReadSets(readsRDDs, sequenceDictionary), allReads.toVector)
+  }
+
+  type TestRead = (String, Int, Int, Int)
+  type TestReads = Seq[TestRead]
+  type TestPos = (String, Int)
+  type ExpectedReadIdx = Int
+  type ExpectedReadIdxs = Iterable[ExpectedReadIdx]
+  type ExpectedReads = String
+  type ExpectedReads3 = (ExpectedReads, ExpectedReads, ExpectedReads)
+  type ExpectedReadIdxs3 = (ExpectedReadIdxs, ExpectedReadIdxs, ExpectedReadIdxs)
+
+  type ExpectedPosReads = (TestPos, ExpectedReads)
+
+  //type ExpectedPosReadsPerSample = (TestPos, PerSample[ExpectedReadIdxs])
+  type ExpectedPosPerSample = (TestPos, ExpectedReads3)
+
+  def checkPileup(contig: Contig,
+                  locus: Int,
+                  reads: Seq[MappedRead],
+                  readsStr: ExpectedReads,
+                  pileup: TestPileup): Unit = {
+    pileup should be(
+      (
+        contig,
+        locus,
+        readsStr
+      )
+    )
   }
 
   def testPileups(halfWindowSize: Int,
@@ -133,47 +153,21 @@ class ReadSetsSuite extends GuacFunSuite with Util {
                   reads: TestReads,
                   expected: Iterable[ExpectedPosReads]): Unit = {
 
-    val (readsets, allReads) = makeReadSets(reads, numPartitions)
+    val (readsets, mappedReads) = makeReadSets(reads, numPartitions)
 
     val pileups =
       readsets
         .pileups(halfWindowSize, maxRegionsPerPartition, reference)
-        .map(pileup =>
-          (
-            pileup.referenceName,
-            pileup.locus,
-            pileup.elements.map(e =>
-              (
-                e.read,
-                e.locus,
-                e.readPosition
-              )
-            ).toArray.sortBy(-_._3).toList
-          )
-        )
+        .map(simplifyPileup)
         .collect()
 
     withClue(s"numPartitions: $numPartitions\n") {
       pileups.length should be(expected.size)
 
       for {
-        (pileup, ((contig, locus), readIndices)) <- pileups.zip(expected)
+        (pileup, ((contig, locus), readsStr)) <- pileups.zip(expected)
       } {
-
-        val expectedReads =
-          for {
-            (readIdx, readPosition) <- readIndices.toList
-          } yield {
-            (allReads(readIdx), locus, readPosition)
-          }
-
-        pileup should be(
-          (
-            contig,
-            locus,
-            expectedReads
-          )
-        )
+        checkPileup(contig, locus, mappedReads, readsStr, pileup)
       }
     }
   }
@@ -194,28 +188,28 @@ class ReadSetsSuite extends GuacFunSuite with Util {
 
     val pileups =
       List(
-        ("chr1",  99) -> List(),
-        ("chr1", 100) -> List((0, 0)),
-        ("chr1", 101) -> List((0, 1), (1, 0)),
-        ("chr1", 102) -> List((0, 2), (1, 1)),
-        ("chr1", 103) -> List((0, 3), (1, 2)),
-        ("chr1", 104) -> List((0, 4), (1, 3)),
-        ("chr1", 105) -> List((1, 4)),
-        ("chr1", 106) -> List(),
-        ("chr2",   7) -> List(),
-        ("chr2",   8) -> List((2, 0)),
-        ("chr2",   9) -> List((3, 0)),
-        ("chr2",  10) -> List((3, 1)),
-        ("chr2",  11) -> List(),
-        ("chr2", 101) -> List(),
-        ("chr2", 102) -> List((4, 0)),
-        ("chr2", 103) -> ((4, 1) :: (5 until 15).toList.map(i => (i, 0))),
-        ("chr2", 104) -> ((4, 2) :: (5 until 15).toList.map(i => (i, 1))),
-        ("chr2", 105) -> (5 until 15).toList.map(i => (i, 2)),
-        ("chr2", 106) -> List(),
-        ("chr5",  89) -> List(),
-        ("chr5",  90) -> (15 until 25).toList.map(i => (i, 0)),
-        ("chr5",  91) -> List()
+        ("chr1",  99) -> "",
+        ("chr1", 100) -> "[100,105)",
+        ("chr1", 101) -> "[100,105), [101,106)",
+        ("chr1", 102) -> "[100,105), [101,106)",
+        ("chr1", 103) -> "[100,105), [101,106)",
+        ("chr1", 104) -> "[100,105), [101,106)",
+        ("chr1", 105) -> "[101,106)",
+        ("chr1", 106) -> "",
+        ("chr2",   7) -> "",
+        ("chr2",   8) -> "[8,9)",
+        ("chr2",   9) -> "[9,11)",
+        ("chr2",  10) -> "[9,11)",
+        ("chr2",  11) -> "",
+        ("chr2", 101) -> "",
+        ("chr2", 102) -> "[102,105)",
+        ("chr2", 103) -> "[102,105), [103,106)*10",
+        ("chr2", 104) -> "[102,105), [103,106)*10",
+        ("chr2", 105) -> "[103,106)*10",
+        ("chr2", 106) -> "",
+        ("chr5",  89) -> "",
+        ("chr5",  90) -> "[90,91)*10",
+        ("chr5",  91) -> ""
       )
 
     test("pileups: small") {
@@ -256,31 +250,18 @@ class ReadSetsSuite extends GuacFunSuite with Util {
     }
   }
 
-
   def testPerSamplePileups(halfWindowSize: Int,
                            maxRegionsPerPartition: Int,
                            numPartitions: Int,
                            reads: PerSample[TestReads],
                            expected: Iterable[ExpectedPosPerSample]): Unit = {
-    val (readsets, allReads) = makeReadSets(reads, numPartitions)
+    val (readsets, mappedReads) = makeReadSets(reads, numPartitions)
 
-    val pileups: Array[PerSample[(String, Long, List[(MappedRead, Long, Int)])]] =
+    val pileups: Array[PerSample[(Contig, Locus, ExpectedReads)]] =
       readsets
         .perSamplePileups(halfWindowSize, maxRegionsPerPartition, reference)
         .map(
-          _.map(pileup =>
-            (
-              pileup.referenceName,
-              pileup.locus,
-              pileup.elements.map(e =>
-                (
-                  e.read,
-                  e.locus,
-                  e.readPosition
-                )
-              ).toArray.sortBy(-_._3).toList
-            )
-          )
+          _.map(simplifyPileup)
         )
         .collect()
 
@@ -295,23 +276,10 @@ class ReadSetsSuite extends GuacFunSuite with Util {
 
         withClue(s"$contig:$locus:\n") {
           for {
-            (((pileup, readIndices), mappedReads), sampleId) <- pileups.zip(reads).zip(allReads).zipWithIndex
+            (((pileup, readsStr), sampleReads), sampleId) <- pileups.zip(reads).zip(mappedReads).zipWithIndex
           } {
-
             withClue(s"sample: $sampleId\n") {
-              val expectedReads =
-                for {
-                  (readIdx, readPosition) <- readIndices.toList
-                } yield
-                  (mappedReads(readIdx), locus, readPosition)
-
-              pileup should be(
-                (
-                  contig,
-                  locus,
-                  expectedReads
-                )
-              )
+              checkPileup(contig, locus, sampleReads, readsStr, pileup)
             }
           }
         }
@@ -344,43 +312,47 @@ class ReadSetsSuite extends GuacFunSuite with Util {
         ("chr2",  40,  42, 3)*/
       )
 
-    val none: ExpectedReadIdxs = Nil
+//    implicit def intToSeqInt(i: Int): List[Int] = List(i)
+    implicit def t1ToE(t: (Int, Iterable[Int], Iterable[Int])): ExpectedReadIdxs3 = (List(t._1), t._2, t._3)
+    implicit def t3ToE(t: (Iterable[Int], Iterable[Int], Int)): ExpectedReadIdxs3 = (t._1, t._2, List(t._3))
+
+    val n: ExpectedReadIdxs = Nil
 
     val pileups: Iterable[ExpectedPosPerSample] =
       List[ExpectedPosPerSample](
-        "chr1" ->  98 -> (none, none, none),
-        "chr1" ->  99 -> (List(), List(), List()),
-        "chr1" -> 100 -> (List(0 -> 0, 1 -> 0), List(), List()),
-        "chr1" -> 101 -> (List(0 -> 1, 1 -> 1), List(), List(0 -> 0)),
-        "chr1" -> 102 -> (List(0 -> 2, 1 -> 2, 2 -> 0, 3 -> 0), List(), List(1 -> 0, 2 -> 0, 3 -> 0, 4 -> 0, 5 -> 0)),
-        "chr1" -> 103 -> (List(2 -> 1, 3 -> 1), List(), List()),
-        "chr1" -> 104 -> (List(), List(), List(6 -> 0, 7 -> 0, 8 -> 0)),
-        "chr1" -> 105 -> (List(), List(), List()),
-        "chr1" -> 106 -> (List(4 -> 0), List(), List()),
-        "chr1" -> 107 -> (List(), List(), List()),
-        "chr1" -> 108 -> (List(), List(), List())/*,
-        "chr2" ->   6 -> (List(), List(), List()),
-        "chr2" ->   7 -> (List(), List(), List()),
-        "chr2" ->   8 -> (List(), List(), List()),
-        "chr2" ->   9 -> (List(), List(), List()),
-        "chr2" ->  18 -> (List(), List(), List()),
-        "chr2" ->  19 -> (List(), List(), List()),
-        "chr2" ->  20 -> (List(), List(), List()),
-        "chr2" ->  21 -> (List(), List(), List()),
-        "chr2" ->  22 -> (List(), List(), List()),
-        "chr2" ->  23 -> (List(), List(), List()),
-        "chr2" ->  28 -> (List(), List(), List()),
-        "chr2" ->  29 -> (List(), List(), List()),
-        "chr2" ->  30 -> (List(), List(), List()),
-        "chr2" ->  31 -> (List(), List(), List()),
-        "chr2" ->  32 -> (List(), List(), List()),
-        "chr2" ->  33 -> (List(), List(), List()),
-        "chr2" ->  38 -> (List(), List(), List()),
-        "chr2" ->  39 -> (List(), List(), List()),
-        "chr2" ->  40 -> (List(), List(), List()),
-        "chr2" ->  41 -> (List(), List(), List()),
-        "chr2" ->  42 -> (List(), List(), List()),
-        "chr2" ->  43 -> (List(), List(), List())*/
+        "chr1" ->  98 -> ("", "", ""),
+        "chr1" ->  99 -> ("", "", ""),
+        "chr1" -> 100 -> ("[100,103)*2", "", ""),
+        "chr1" -> 101 -> ("[100,103)*2", "", "[101,102)"),
+        "chr1" -> 102 -> ("[100,103)*2, [102,104)*2", "", "[102,103)*5"),
+        "chr1" -> 103 -> ("[102,104)*2", "", ""),
+        "chr1" -> 104 -> ("", "", "[104,105)*3"),
+        "chr1" -> 105 -> ("", "", ""),
+        "chr1" -> 106 -> ("[106,107)", "", ""),
+        "chr1" -> 107 -> ("", "", ""),
+        "chr1" -> 108 -> ("", "", "")/*,
+        "chr2" ->   6 -> ("", "", ""),
+        "chr2" ->   7 -> ("", "", ""),
+        "chr2" ->   8 -> ("", "", ""),
+        "chr2" ->   9 -> ("", "", ""),
+        "chr2" ->  18 -> ("", "", ""),
+        "chr2" ->  19 -> ("", "", ""),
+        "chr2" ->  20 -> ("", "", ""),
+        "chr2" ->  21 -> ("", "", ""),
+        "chr2" ->  22 -> ("", "", ""),
+        "chr2" ->  23 -> ("", "", ""),
+        "chr2" ->  28 -> ("", "", ""),
+        "chr2" ->  29 -> ("", "", ""),
+        "chr2" ->  30 -> ("", "", ""),
+        "chr2" ->  31 -> ("", "", ""),
+        "chr2" ->  32 -> ("", "", ""),
+        "chr2" ->  33 -> ("", "", ""),
+        "chr2" ->  38 -> ("", "", ""),
+        "chr2" ->  39 -> ("", "", ""),
+        "chr2" ->  40 -> ("", "", ""),
+        "chr2" ->  41 -> ("", "", ""),
+        "chr2" ->  42 -> ("", "", ""),
+        "chr2" ->  43 -> ("", "", "")*/
       )
 
     test("per-sample pileups") {

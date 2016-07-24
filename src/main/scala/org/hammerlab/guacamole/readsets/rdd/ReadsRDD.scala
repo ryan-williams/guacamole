502f206a88629a2fa4d9a867765a00763e20048f
Ryan Williams <ryan.blake.williams@gmail.com>
2016-07-24 07:39:52 +0000

    name some RDDs

--- src/main/scala/org/hammerlab/guacamole/readsets/rdd/ReadsRDD.scala
+++ src/main/scala/org/hammerlab/guacamole/readsets/rdd/ReadsRDD.scala
@@ -1,5 +1,7 @@
 package org.hammerlab.guacamole.readsets.rdd
 
+import java.io.File
+
 import org.apache.spark.rdd.RDD
 import org.hammerlab.guacamole.reads.{MappedRead, PairedRead, Read}
 import org.hammerlab.guacamole.readsets.loading.Input
@@ -9,16 +11,19 @@ import org.hammerlab.guacamole.readsets.loading.Input
  */
 case class ReadsRDD(reads: RDD[Read], input: Input) {
 
+  val basename = new File(input.path).getName
+  val shortName = basename.substring(0, math.min(100, basename.length))
+
   lazy val mappedReads =
-    reads.flatMap {
+    reads.flatMap({
       case r: MappedRead                   => Some(r)
       case PairedRead(r: MappedRead, _, _) => Some(r)
       case _                               => None
-    }
+    }).setName(s"Mapped reads: $shortName")
 
   lazy val mappedPairedReads: RDD[PairedRead[MappedRead]] =
-    reads.flatMap {
+    reads.flatMap({
       case rp: PairedRead[_] if rp.isMapped => Some(rp.asInstanceOf[PairedRead[MappedRead]])
       case _                                => None
-    }
+    }).setName(s"Mapped reads: $shortName")
 }
